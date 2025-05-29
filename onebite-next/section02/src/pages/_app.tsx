bd45da74460a5f2bac1214bd75a2837275a5b27c
arty0928 <arty0928@konkuk.ac.kr>
2025-05-29 21:30:07 +0900

    [2.4] prefetching

--- onebite-next/section02/src/pages/_app.tsx
+++ onebite-next/section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 // 모든 페이지 역할을 하는 컴포넌트의 부모 컴포넌트
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,12 +12,19 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push("/test");
   };
 
+  //useEffect: 화면에 mount 될때
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>
         <Link href={"/"}>index</Link>
         &nbsp;
-        <Link href={"/search"}>search</Link>
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
         &nbsp;
         <Link href={"/book/1"}>book/1</Link>
         <div>
