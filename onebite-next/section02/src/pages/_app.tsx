064ce7dbb4e1e3661c5028af3b9b13625d5dd856
arty0928 <arty0928@konkuk.ac.kr>
2025-05-29 21:15:36 +0900

    [2.3] 네비게이팅

--- onebite-next/section02/src/pages/_app.tsx
+++ onebite-next/section02/src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 // 모든 페이지 역할을 하는 컴포넌트의 부모 컴포넌트
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
   return (
     <>
-      <header>Global header</header>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+        &nbsp;
+      </header>
       <Component {...pageProps} />
     </>
   );
