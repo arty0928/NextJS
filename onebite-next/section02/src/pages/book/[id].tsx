717ab111c9ee7a06d40c914558b3524b206b4dd4
arty0928 <arty0928@konkuk.ac.kr>
2025-06-01 01:51:24 +0900

    [2.19] SEO 설정하기

--- onebite-next/section02/src/pages/book/[id].tsx
+++ onebite-next/section02/src/pages/book/[id].tsx
@@ -9,6 +9,7 @@ import {
 import fetchOneBook from "@/lib/fetch-one-book";
 import { useRouter } from "next/router";
 import { notFound } from "next/navigation";
+import Head from "next/head";
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
@@ -32,7 +33,7 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
   - blocking : 즉시 생성 후 반환(like SSR) , 
         주의 : 존재하지 않은 페이지를 SSR 방식으로 요청할 때 사전 렌더링 시간이 길어지면 로딩이 길어질 수 있음
               -> True 옵션으로 해결 
-  - true : 1. 일단 Props 없는 페이지만 미리 반환 
+  - true : 1. 일단 데이터 없는 페이지만 미리 반환 
            2. 즉시 생성 후 Props 만 따로 반환, 데이터가 있는 상태의 페이지 렌더링
 */
 export const getStaticPaths = () => {
@@ -47,24 +48,44 @@ export const getStaticPaths = () => {
 export default function Page({ book }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  if (router.isFallback) return "로딩 중입니다";
+  if (router.isFallback) {
+    return (
+      <>
+        <Head>
+          <title>한입북스</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="한입북스" />
+          <meta property="og:description" content="한입북스" />
+        </Head>
+        <div>로딩중입니다.</div>
+      </>
+    );
+  }
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } = book;
   return (
-    <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
-      >
-        <img src={coverImgUrl} />
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={coverImgUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.cover_img_container}
+          style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        >
+          <img src={coverImgUrl} />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
       </div>
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>
-        {author} | {publisher}
-      </div>
-      <div className={style.description}>{description}</div>
-    </div>
+    </>
   );
 }
--- onebite-next/section02/src/pages/search/index.tsx
+++ onebite-next/section02/src/pages/search/index.tsx
@@ -9,6 +9,7 @@ import {
 import fetchBooks from "@/lib/fetch-books";
 import { useRouter } from "next/router";
 import { BookData } from "../../../public/types";
+import Head from "next/head";
 
 // export const getStaticProps = async (context: GetStaticPropsContext) => {
 //   const q = context.query.q;
@@ -39,6 +40,12 @@ export default function Page() {
 
   return (
     <div>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요." />
+      </Head>
       {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
