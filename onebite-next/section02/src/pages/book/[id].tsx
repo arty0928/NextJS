a357dcbdf8ac47c2989dadb6833f49cb80a0e0c5
arty0928 <arty0928@konkuk.ac.kr>
2025-06-01 00:41:34 +0900

    [2.16] SSG 4. 폴백옵션 설정하기

--- onebite-next/section02/src/pages/book/[id].tsx
+++ onebite-next/section02/src/pages/book/[id].tsx
@@ -7,26 +7,47 @@ import {
   InferGetStaticPropsType,
 } from "next";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
+import { notFound } from "next/navigation";
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  // 데이터를 불러오지 못하면 notFound 화면
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: { book },
   };
 };
 
+/*
+  fallback 옵션
+  - false : 404 Not Found 반환
+  - blocking : 즉시 생성 후 반환(like SSR) , 
+        주의 : 존재하지 않은 페이지를 SSR 방식으로 요청할 때 사전 렌더링 시간이 길어지면 로딩이 길어질 수 있음
+              -> True 옵션으로 해결 
+  - true : 1. 일단 Props 없는 페이지만 미리 반환 
+           2. 즉시 생성 후 Props 만 따로 반환, 데이터가 있는 상태의 페이지 렌더링
+*/
 export const getStaticPaths = () => {
   return {
     //문자열 파라미터의 값은 항상 "문자열"
     paths: [{ params: { id: "1" } }, { params: { id: "2" } }, { params: { id: "3" } }],
     // paths에 없는 파라미터를 보낼 때 대비책
-    fallback: false,
+    fallback: true,
   };
 };
 
 export default function Page({ book }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) return "로딩 중입니다";
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } = book;
